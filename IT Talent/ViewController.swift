e9a5bd05eac99b8f9f0d49d51dfc7f23da056d42
Mauricio Garcia <mauricaz@outlook.com>
2023-01-27 21:45:29 -0600

    Initial Commit

---
+++ IT Talent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IT Talent
+//
+//  Created by Mauricio García S on 27/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
